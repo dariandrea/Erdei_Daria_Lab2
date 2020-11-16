41d6dcfac7a8ddada674371a668796f6a9b32943
Daria Erdei <dariandrea@yahoo.com>
2020-11-16 12:54:48 +0200

    Laboratorul 4

---
+++ StopCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+namespace Erdei_Daria_Lab2.CustomCommands
+{
+    class StopCommands
+    {
+        private static RoutedUICommand launch_command;
+        static StopCommands()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch",
+           typeof(StopCommands), myInputGestures);
+        }
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
